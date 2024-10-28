a76e3adc866c50638570b9e1bf0cac8b35b2df50
MaximilianMcC <max@maximilian.co.nz>
2024-10-29 10:53:16 +1300

    establishment comes up in the list

--- src/public/script/search.js
+++ src/public/script/search.js
@@ -12,5 +12,56 @@ searchBar.addEventListener("keydown", async (e) => {
 	
 	// Get all of the responses
 	const establishments = await httpGet(url);
-	console.log(establishments);
+
+	// Clear all the previous stuff so we can
+	// put all the new stuff in
+	// TODO: Do this
+	document.querySelector(".cards").innerHTML = ``;
+
+	// Add all the new stuff in
+	establishments.forEach(establishment => {
+		addEstablishmentCard(establishment);
+		addEstablishmentToMap(establishment);
+	});
 });
+
+function addEstablishmentCard(establishment) {
+	
+	let html = `
+	<div class="establishment-card">
+		<h2>${establishment["name"]}</h2>
+		<div class="features">
+			<ul>
+				<li>Fully customizable</li>
+				<li>Has items on your whitelist</li>
+				<li>Simple items available</li>
+			</ul>
+		</div>
+		<div class="info">
+
+			<!-- Buttons to get the menu, and website  -->
+			<button>Menu</button>
+			<a href="${establishment["website"]}">Website</a>
+
+			<!-- Thing showing average rating in terms of 5 stars -->
+			<div class="star-container">
+				<p>
+					<i class="fa-solid fa-star"></i>
+					<i class="fa-solid fa-star"></i>
+					<i class="fa-solid fa-star"></i>
+					<i class="fa-solid fa-star"></i>
+					<i class="fa-solid fa-star"></i>
+				</p>
+			</div>
+		</div>
+	</div>
+	`;
+
+	document.querySelector(".cards").innerHTML += html;
+}
+
+// TODO: Put in map.js
+function addEstablishmentToMap(establishment) {
+	
+
+}
